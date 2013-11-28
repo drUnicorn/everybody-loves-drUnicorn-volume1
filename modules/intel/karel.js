f1110a3a40f080ead7c7a8f1ad5da3a2d753a4b6
DavidNovak <danov.dk.88@gmail.com>
2013-11-28 18:15:47 +0100

    hexaKarel - zdi
    Přidání a odebrání "zdí".

--- modules/intel/karel.js
+++ modules/intel/karel.js
@@ -4,28 +4,28 @@
   var y     = parseInt(doc.currentScript.getAttr("data-y"));
   var smer  = parseInt(doc.currentScript.getAttr("data-start-smer"));
   win.karel = {}
-  
+ 
   var style = doc.mkNode("link");
   style.setAttr("rel","stylesheet");
   style.setAttr("href","./modules/intel/karel.css");
   doc.head.addChild(style);
-  
+ 
   var red = new CellNode("red");
   var blk = new CellNode("black");
   var krl = new CellNode("karel");
-  
+ 
   krl.xy.place(x,y);
-  
+
   win.karel.krok = function(){
-    if(tmp=krl.cells[0].dir(smer)){
-      tmp=tmp.xy();
-      krl.remove(); 
-      krl.xy.place(tmp[0],tmp[1]);
-    }
-    else {
-      alert("Narazil jsem! Au!")
-    }
-  }  
+      if(tmp=krl.cells[0].dir(smer)){
+        tmp=tmp.xy();
+        krl.remove(); 
+        krl.xy.place(tmp[0],tmp[1]);
+      }else{
+        alert("Narazil jsem! Au!");
+      }
+    }  
+  
   win.karel.vlevo_vbok = function(){
     smer--;       
   }
@@ -44,7 +44,7 @@
     if(blk.cells.indexOf(World.xy(xy[0],xy[1]))+1){
       blk.remove(xy[0],xy[1]);
     }else{
-      alert("Není co zvednout!")
+      alert("Není co zvednout!");
     }
   } 
   
@@ -52,9 +52,15 @@
   
   cells = doc.getElementsByTagName("cell");
   i = cells.length;
-  while(i-->0){
-   cells[i].on("click",function(e){
-    cell = e.target.mapNode;
-   })
+  element = cells
+  while(i-->0)  {
+    cells[i].on("click",function(e){                     
+      cell = e.target.mapNode;
+      if(red.cells.indexOf(World.xy(cell.xy()[0],cell.xy()[1]))+1){
+        red.remove(cell.xy()[0],cell.xy()[1]);
+      }else{
+        red.xy.place(cell.xy()[0],cell.xy()[1]);
+      }
+    });
   }
-})()
+})();
